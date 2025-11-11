a92629469a391e9ba38c3ff582496fc7761792af
EduardBogoslovtcev2 <ebogoslovtcev@rrc.ca>
2025-11-11 17:23:22 -0600

    add javascript to change between login and register

---
+++ showForm.js
@@ -0,0 +1,6 @@
+function showForm(formID) {
+    document
+        .querySelectorAll(".form-box")
+        .forEach((form) => form.classList.remove("active"));
+    document.getElementById(formID).classList.add("active");
+}
